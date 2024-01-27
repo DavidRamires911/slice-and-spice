3a7205529a49be6dd774ff41272da37ebc696353
DavidCorreia911 <davidcorreia911@gmail.com>
2024-01-27 15:26:03 +0000

    initial layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+    <Link   to="/">Slice & Spice</Link>
+    
+    <p>David Ramirez</p>
+    
+    </div>
+  )
+}
+
+export default Header
